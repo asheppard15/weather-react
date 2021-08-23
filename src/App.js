ecc06aee55ccdd4d0d7f417643f678d034edf24d
asheppard15 <83742214+asheppard15@users.noreply.github.com>
2021-08-23 12:59:55 -0400

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 
 export default function App() {
   return <div className="App">
+    <div className="container">
       <h1>Weather App </h1>
-      <footer> This project was coded by Andrea Sheppard and is{" "}<a href="https://www.github.com/asheppard14/weather-react" target="blank">open-sourced on GitHub</a></footer>
+      <footer> This project was coded by Andrea Sheppard and is{" "}<a href="https://github.com/asheppard14/weather-react" target="blank">open-sourced on GitHub</a></footer>
+    </div>
     </div>;
 }
 
