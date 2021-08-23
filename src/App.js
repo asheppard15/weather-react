0ed5e3a5cd01ac67e993a19190370245eee8d210
asheppard15 <83742214+asheppard15@users.noreply.github.com>
2021-08-23 11:11:35 -0400

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 export default function App() {
   return <div className="App">
-      Weather App
+      <h1>Weather App </h1>
+      <footer> This project was coded by Andrea Sheppard and is{" "}<a href="https://www.github.com/asheppard14/weather-react" target="blank">open-sourced on GitHub</a></footer>
     </div>;
 }
 
