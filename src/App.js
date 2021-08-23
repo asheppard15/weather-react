298d53845e3885af04a3061aaab5d69daca93ecd
asheppard15 <83742214+asheppard15@users.noreply.github.com>
2021-08-23 16:13:09 -0400

    Integrated API data

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Weather from "./Weather";
 export default function App() {
   return <div className="App">
     <div className="container">
-      <h1>Weather App </h1>
+      <Weather defaultCity="Paris" />
       <footer> This project was coded by Andrea Sheppard and is{" "}<a href="https://github.com/asheppard14/weather-react" target="blank">open-sourced on GitHub</a></footer>
     </div>
     </div>;
