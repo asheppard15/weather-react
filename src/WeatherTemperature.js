b1d4243449d14dae428210204c852f0d87b40a9e
asheppard15 <83742214+asheppard15@users.noreply.github.com>
2021-08-24 15:54:40 -0400

    Integrated API

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -2,24 +2,34 @@ import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
     const [unit, setUnit] = useState("celsius");
-    function convertToFahrenheit(event) {
+    function showFahrenheit(event) {
         event.preventDefault();
         setUnit("fahrenheit");
     }
+
+    function showCelsius(event) {
+        event.preventDefault();
+        setUnit("celsius");
+    }
+
+    function fahrenheit() {
+        return (props.celsius * 9) / 5 + 32;
+    }
+
     if (unit === 'celsius') {
     return (
         <div className="WeatherTemperature">
           <span className="temperature">
             {Math.round(props.celsius)}
           </span>      
-          <span className="unit">℃ |{" "}<a href="/" onClick={convertToFahrenheit}>℉</a> </span>
+          <span className="unit">℃ |{" "}<a href="/" onClick={showFahrenheit}>℉</a> </span>
         </div>
     )
 } else {
     return (
         <div className="WeatherTemperature">
           <span className="temperature">
-            {Math.round(props.celsius)}
+            {Math.round(fahrenheit)}
           </span>      
           <span className="unit"><a href="/" onClick={convertToCelsius}>℃</a>{" "}| ℉</span>
         </div>
