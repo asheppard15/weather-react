2d340af8b191487d4ba8a286a1a476e7b23177ea
asheppard15 <83742214+asheppard15@users.noreply.github.com>
2021-08-25 13:47:07 -0400

    Applied finishing touches

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return <div className="App">
     <div className="container">
       <Weather defaultCity="Paris" />
-      <footer> This project was coded by Andrea Sheppard and is{" "}<a href="https://github.com/asheppard14/weather-react" target="blank">open-sourced on GitHub</a></footer>
+      <footer> This project was coded by Andrea Sheppard and is{" "}<a href="https://github.com/asheppard14/weather-react" target="blank" rel="noopener noreferrer">open-sourced on GitHub</a></footer>
     </div>
     </div>;
 }
